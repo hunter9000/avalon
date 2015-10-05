84a5e1b8023b8d236a5c3aad366047b1ba84ba08
Hunter <hunter9000@gmail.com>
2015-10-05 01:04:36 -0400

    select materials in crafting screen and preview effects

--- src/main/java/com/test/model/RecipeRequirementModel.java
+++ src/main/java/com/test/model/RecipeRequirementModel.java
@@ -1,10 +1,13 @@
 package com.test.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 
 // links a material to a recipe as a required mat, and gives the quant
 @Entity
 @Table(name="recipe_requirement")
+@JsonIgnoreProperties(value = "recipe")
 public class RecipeRequirementModel {
 
     @Id
@@ -15,9 +18,9 @@ public class RecipeRequirementModel {
     @JoinColumn(name="recipe_id")     // material_id is the fk column pointing to material table
     private RecipeModel recipe;
 
-//    @OneToOne
-//    @JoinColumn(name="material_id", )
-//    private MaterialModel mat;
+    @OneToOne
+    @JoinColumn(name="material_id")
+    private MaterialModel materialModel;
 
     @Column(name="quantity")
     private int quantity;
@@ -30,12 +33,12 @@ public class RecipeRequirementModel {
         this.recipe = recipe;
     }
 
-//    public MaterialModel getMat() {
-//        return mat;
-//    }
-//    public void setMat(MaterialModel mat) {
-//        this.mat = mat;
-//    }
+    public MaterialModel getMaterialModel() {
+        return materialModel;
+    }
+    public void setMaterialModel(MaterialModel mat) {
+        this.materialModel = mat;
+    }
 
     public int getQuantity() {
         return quantity;
--- src/main/resources/public/js/crafting-controller.js
+++ src/main/resources/public/js/crafting-controller.js
@@ -2,9 +2,10 @@
 
 
     scotchApp.controller('craftingController', function($scope, $http, $window, $routeParams) {
-        $scope.recipes;// = ['Volvo', 'Saab', 'Mercedes', 'Audi'];
+        $scope.recipes;
+        $scope.char;
 
-        $scope.mats;// = ['one', 'two', 'three'];
+        $scope.mats;
         $scope.selectedMat = null;
 
         $scope.baseMats = [];
@@ -27,43 +28,103 @@
             );
         }
         $scope.fetchRecipes();
+        $http({
+            method: 'GET',
+            url: '/api/char/',
+            headers: {'x-access-token': $window.localStorage['jwtToken']}
+        })
+        .success(function (data) {
+            $scope.char = data;
+            $scope.resetMats();
+        })
+        .error(function (data) {
+            console.log(data);
+        });
 
         $scope.recipeSelect = function(recipe) {
 //            console.log($scope.item);
             console.log('controller says this recipe selected ' + recipe);
-            $scope.selectedRecipe = recipe;
+            $scope.resetMats();      // reset the working list of mats
+
+            if ($scope.recipeCraftable(recipe)) {
+                console.log('recipe craftable');
+                $scope.selectedRecipe = recipe;
+
+                // remove all the reqs from the working list of mats
+                for (var i = 0; i < recipe.recipeReqs.length; i++) {
+                    var mat = recipe.recipeReqs[i];
+                    $scope.removeFromList($scope.mats, mat.materialModel.name, mat.quantity);
+                }
+            }
+            else {
+                console.log('recipe not craftable');
+                $scope.selectedRecipe = null;
+            }
         }
 
-        $scope.matSelect = function() {
-            console.log($scope.item);
-            $scope.selectedMat = $scope.item;
+        $scope.matSelect = function(element) {
+            console.log(element);
+            $scope.selectedMat = element;
         }
         $scope.matDblclick = function() {
             console.log($scope.item);
             $scope.extraMats.push($scope.item);
         }
 
-        $scope.listContains = function(list, name, quantity) {
-            list.forEach(function(mat) {
-                if (mat.name == name) {
-                    // check if quantity <= mat.quantity
-                    if (true) {
-                        return true;
-                    }
-                }
-            });
+        $scope.addMatToRecipe = function() {
+            console.log('add to recipe');
         }
 
-        $scope.removeFromList = function(list, material) {
-            list.forEach(function(mat) {
-                if (mat.name == material.name) {
-                    // check if quantity <= mat.quantity
-                    mat.quantity -= material.quantity;
-                    if (mat.quantity == 0) {
-                        // remove from list
+        $scope.filterEffects = function(effectList) {
+            var array = [];
+            if ($scope.selectedRecipe != null) {
+                for (i=0; i<effectList.length; i++) {
+                    if (effectList[i].slot == $scope.selectedRecipe.item.bodySlot) {
+                        array.push(effectList[i]);
                     }
                 }
-            });
+            }
+            return array;
+        }
+
+        $scope.listContains = function(list, name, quantity) {
+            for (var i = 0; i < list.length; i++) {
+                var mat = list[i];
+                if (mat.materialModel.name == name) {
+                    return (quantity <= mat.quantity);      // check if quantity <= mat.quantity
+                }
+            }
+            return false;
+        }
+
+        /** removes the quantity of the named material from the given list. returns boolean if it was removed. */
+        $scope.removeFromList = function(list, name, quantity) {
+            for (var i = 0; i < list.length; i++) {
+                var mat = list[i];
+                if (mat.materialModel.name == name) {
+                    mat.quantity -= quantity;       // reduce the quantity
+                    if (mat.quantity == 0) {
+                        list.splice(i, 1);      // remove from array completely
+                    }
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /** check the given recipe to see if the player's inventory of materials has. returns boolean */
+        $scope.recipeCraftable = function(recipe) {
+            for (i=0; i<recipe.recipeReqs.length; i++) {
+                var req = recipe.recipeReqs[i];
+                if (!$scope.listContains($scope.char.inventoryMaterialModels, req.materialModel.name, req.quantity) ) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        $scope.resetMats = function() {
+            $scope.mats = JSON.parse(JSON.stringify($scope.char.inventoryMaterialModels))
         }
 
     });
